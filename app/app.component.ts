aae7a47a2d9bfae607a61ba7f5d4dece02e1519b
gargtushar <tusshargarg23@gmail.com>
2016-10-23 15:31:37 +0530

    Adding Multiple Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-export class Hero{
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 1, name: 'Mark Evans' },
@@ -20,21 +17,15 @@ const HEROES: Hero[] = [
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-             <h2>Inajuma Eleven</h2>
+  template: `
+             <h1>{{title}}</h1>
+             <h2>Inazuma Eleven</h2>
              <ul class="heroes">
                 <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
                    <span class="badge">{{hero.id}}</span> {{hero.name}}
                 </li>
              </ul>
-             <div *ngIf="selectedHero">
-               <h2>{{selectedHero.name}} details!</h2>
-               <div><label>id: </label>{{selectedHero.id}}</div>
-               <div>
-                 <label>name: </label>
-                 <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-               </div>
-             </div>
+             <my-hero-detail [hero]="selectedHero"></my-hero-detail>
              `,
   styles: [`
              .selected {
